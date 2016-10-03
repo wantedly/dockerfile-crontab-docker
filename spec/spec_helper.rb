188fc2646675a263ef5dac5cc4030829fbdae4c2
Daisuke Fujita <dtanshi45@gmail.com>
2016-10-03 16:12:47 +0900

    Modify serverspec test to run correctly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require 'serverspec'
+require 'docker'
 
 set :backend, :docker
 set :docker_url, ENV['DOCKER_HOST']
-set :docker_container, ENV['TARGET_CONTAINER_ID']
+set :docker_image, "quay.io/wantedly/crontab-docker:latest"
+set :docker_container_create_options, {
+  "Binds" => ["#{File.join(__FILE__, "..", "testdata", "crontab.txt")}:/app/crontab.txt"],
+  "Cmd" => "/app/crontab.txt",
+}
+
+Excon.defaults[:ssl_verify_peer] = false
